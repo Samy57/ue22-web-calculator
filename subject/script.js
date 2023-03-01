fa33b1b614c88893bf9a05eed09296ab76c1b16d
PEGEOT_Sam <sam.pegeot@etu.minesparis.psl.eu>
2023-03-01 16:58:54 +0100

    Fin de séance 1/3/2023

--- subject/script.js
+++ subject/script.js
@@ -3,3 +3,80 @@
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+
+// Sam PEGEOT
+// GitHub Samy57
+
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+  let currentNumberIsDecimal = false;
+  let stack = 0;
+  let action = (a) => (a);
+
+  for (let b of document.getElementsByClassName("calculator__keys")[0].children) {
+    b.addEventListener("click", () => {
+      if (b.dataset.action == "calculate") {
+        document.getElementsByClassName("calculator__display")[0].textContent = action(stack, parseFloat(document.getElementsByClassName("calculator__display")[0].textContent));
+
+      }
+      else if (b.dataset.action == "clear") { // Remise à zéro du nombre tapé
+        document.getElementsByClassName("calculator__display")[0].textContent = "0";
+        currentNumberIsDecimal = false;
+      }
+      else if (b.dataset.action == "decimal") {
+        if (!currentNumberIsDecimal) {
+          currentNumberIsDecimal = true;
+          document.getElementsByClassName("calculator__display")[0].textContent += b.textContent;
+        }
+      }
+
+      else if (b.dataset.action == "multiply") {
+        stack = parseFloat(document.getElementsByClassName("calculator__display")[0].textContent);
+        document.getElementsByClassName("calculator__display")[0].textContent = "0";
+        currentNumberIsDecimal = false;
+        action = (a, b) => (a * b);
+      }
+
+      else if (b.dataset.action == "add") {
+        stack = parseFloat(document.getElementsByClassName("calculator__display")[0].textContent);
+        document.getElementsByClassName("calculator__display")[0].textContent = "0";
+        currentNumberIsDecimal = false;
+        action = (a, b) => { return a + b };
+      }
+      else if (b.dataset.action == "soustract") {
+        stack = parseFloat(document.getElementsByClassName("calculator__display")[0].textContent);
+        document.getElementsByClassName("calculator__display")[0].textContent = "0";
+        currentNumberIsDecimal = false;
+        action = (a, b) => { return a - b };
+      }
+      else if (b.dataset.action == "divide") {
+        stack = parseFloat(document.getElementsByClassName("calculator__display")[0].textContent);
+        document.getElementsByClassName("calculator__display")[0].textContent = "0";
+        currentNumberIsDecimal = false;
+        action = (a, b) => { return a / b };
+      }
+
+      else if (b.dataset.action == "number") { // C'est un chiffre
+        if (document.getElementsByClassName("calculator__display")[0].textContent == "0") { // Traite le cas particulier des zéros
+          if (!(b.textContent == "0")) {
+            document.getElementsByClassName("calculator__display")[0].textContent = b.textContent;
+          }
+        }
+        else {
+          document.getElementsByClassName("calculator__display")[0].textContent += b.textContent;
+        }
+      }
+    }
+    );
+
+
+
+  }
+
+
+});
+
+
